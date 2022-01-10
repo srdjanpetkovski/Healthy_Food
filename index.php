d5d2a9dc1b373e0e6d63accc43510bb0b80cc685
srdjanpetkovski <srdjanpetkovski@gmail.com>
2022-01-10 20:37:32 +0100

    index - footer html

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="style.css">
+    <title>Healthy Food</title>
+</head>
+<body>
+    <!-- nav -->
+    <nav class="container">
+        <a class="logo" href="index.php">Healthy <span>Food</span></a>
+        <ul>
+            <li><a href="index.php">Naslovna</a></li>
+            <li><a href="plan.php">Plan</a></li>
+            <li><a href="Proizvodnja.php">Proizvodnja</a></li>
+        </ul>
+    </nav>
+
+    <!-- index Header -->
+    <header class="indexHeader">
+        <article class="container">
+            <hgroup>
+                <h1>Odaberite zdravlje</h1>
+                <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, quidem corporis? Minima maxime sed in
+                molestias! Laborum assumenda eaque libero.</h4>
+                <button>Procitaj vise</button>
+            </hgroup>
+        </article>
+    </header>
+
+    <!-- about -->
+    <section class="about container">
+        <article>
+            <h2>Koraci do <span>zdravlja</span></h2>
+            <h3>01</h3>
+            <h4>Dole ugljeni hidrati</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum possimus reprehenderit reiciendis distinctio a est odit animi, enim aliquam quos earum maiores tempora architecto commodi vitae esse blanditiis illo. Cupiditate!</p>
+        </article>
+        <article>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae iusto voluptates, repudiandae voluptas enim eligendi magni accusantium delectus asperiores dolores.</p>
+            <h3>02</h3>
+            <h4>Gore proteini</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum possimus reprehenderit reiciendis distinctio a est odit animi, enim aliquam quos earum maiores tempora architecto commodi vitae esse blanditiis illo. Cupiditate!</p>
+        </article>
+    </section>
+
+    <!-- footer -->
+    <section class="footer">
+        <article>
+            <h4>Kontaktirajte nas za plan ishrane</h4>
+            <p>Zmaj Jovina 23<br>21000 Novi Sad</p>
+        </article>
+        <article>
+            <h2>Kontakt</h2>
+            <ul>
+                <li><i class="fas fa-phone"></i>021 472 491</li>
+                <li><i class="fas fa-mobile"></i>062 214 454</li>
+                <li><i class="fas fa-envelope"></i>info@healthyfood.com</li>
+            </ul>
+        </article>
+    </section>
+</body>
+</html>
